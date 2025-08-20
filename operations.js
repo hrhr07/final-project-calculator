c3924b415e8d897265b91aa8db6577a828048b95
Hector Hernandez <hectorrhr07@gmail.com>
2025-08-20 11:09:06 -0600

    create calc operations and other files

---
+++ operations.js
@@ -0,0 +1,37 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (arr) {
+  return arr.reduce((a, b) => a + b, 0);
+};
+
+const multiply = function (arr) {
+  return arr.reduce((a, b) => a * b, 1);
+};
+
+const power = function (a, b) {
+  return Math.pow(a, b);
+};
+
+const factorial = function (a) {
+  if (a === 0 || a === 1) return 1;
+  for (var i = a - 1; i >= 1; i--) {
+    a *= i;
+  }
+  return a;
+};
+
+
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
